33a367d95acc5ae3ac4c7729e6968bdb903897c1
Maharshi Rawal <rawalmaharshi@gmail.com>
2020-05-25 11:13:08 -0400

    Updates

---
+++ precache-manifest.7e32ad7133ce6ee21ed7e6c2c46e7ee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4771c10862dd0ebe8750acbfe3caed",
+    "url": "/react-app-from-create-app/index.html"
+  },
+  {
+    "revision": "5bb05062f7b87028ca9f",
+    "url": "/react-app-from-create-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e0c5cdb2185d361f300f",
+    "url": "/react-app-from-create-app/static/js/2.50b4e5e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-from-create-app/static/js/2.50b4e5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb05062f7b87028ca9f",
+    "url": "/react-app-from-create-app/static/js/main.3228ffc2.chunk.js"
+  },
+  {
+    "revision": "5baf06f758e4b4984e7f",
+    "url": "/react-app-from-create-app/static/js/runtime-main.0f742019.js"
+  }
+]);
